2c9e36b5a78542f595e0959f67d47bbbc5ef0a6d
snehag62127 <118705053+snehag62127@users.noreply.github.com>
2023-10-30 13:49:16 +0530

    added convert button

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ let celciusInput = document.querySelector('#celcius > input')
 let farheniteInput = document.querySelector('#farhenite > input')
 let kelvinInput = document.querySelector('#kelvin > input')
 
-let btn=document.querySelector('.button button')
+let btn=document.getElementById('clear-btn')
+let convertBtn=document.getElementById('convert-btn')
 
 function roundNumber(number) {
     return Math.round(number*100)/100
@@ -13,8 +14,11 @@ celciusInput.addEventListener('input',function(){
     let fTemp=(cTemp*(9/5))+32
     let kTemp=cTemp+273.15
 
-    farheniteInput.value=roundNumber(fTemp)
-    kelvinInput.value=roundNumber(kTemp)
+    convertBtn.addEventListener('click',()=>{
+        celciusInput.value=roundNumber(cTemp)
+        farheniteInput.value=roundNumber(fTemp)
+        kelvinInput.value=roundNumber(kTemp)
+    })
 })
 //farhenite to celcius and kelvin
 farheniteInput.addEventListener('input',function(){
@@ -22,8 +26,11 @@ farheniteInput.addEventListener('input',function(){
     let cTemp=(fTemp-32)*(5/9)
     let kTemp=cTemp+273.15
 
-    celciusInput.value=roundNumber(cTemp)
-    kelvinInput.value=roundNumber(kTemp)
+    convertBtn.addEventListener('click',()=>{
+        celciusInput.value=roundNumber(cTemp)
+        farheniteInput.value=roundNumber(fTemp)
+        kelvinInput.value=roundNumber(kTemp)
+    })
 })
 //kelvin to farhenite and celcius
 kelvinInput.addEventListener('input',function(){
@@ -31,8 +38,11 @@ kelvinInput.addEventListener('input',function(){
     let cTemp=kTemp-273.15
     let fTemp=(cTemp*(9/5))+32
     
-    celciusInput.value=roundNumber(cTemp)
-    farheniteInput.value=roundNumber(fTemp)
+    convertBtn.addEventListener('click',()=>{
+        celciusInput.value=roundNumber(cTemp)
+        farheniteInput.value=roundNumber(fTemp)
+        kelvinInput.value=roundNumber(kTemp)
+    })
 })
 
 btn.addEventListener('click',()=>{
